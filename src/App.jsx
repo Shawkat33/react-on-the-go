01a9c0a263546668a85d6608fe66ba37e8b71c6e
Shawkat Islam <shawkat.islam33@gmail.com>
2025-10-01 16:09:03 +0600

    React simple project start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Countries from "./Components/Countries/countries";
+
+function App() {
+	return (
+		<>
+			<Countries></Countries>
+		</>
+	);
+}
+
+export default App;
